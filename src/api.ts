f9f458fa662181c5be73ca5d2389082495bc2abc
조현일 <himzei@gmail.com>
2023-01-01 08:24:46 +0900

    development

--- src/api.ts
+++ src/api.ts
@@ -3,10 +3,7 @@ import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:8000/api/v1/"
-      : "https://airbnbproject.onrender.com/",
+  baseURL: "https://airbnbproject.onrender.com/",
   withCredentials: true,
 });
 
