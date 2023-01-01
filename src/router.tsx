aef65cb2c8cf7aa8d33bb4880bdee3a117c0324d
조현일 <himzei@gmail.com>
2023-01-01 09:59:00 +0900

    model url

--- src/router.tsx
+++ src/router.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
-        path: "motel",
+        path: "motels",
         element: <Motel />,
       },
       {
